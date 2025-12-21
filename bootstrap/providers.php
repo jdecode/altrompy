18f3b6e6815b1c26eb07b24c7a1476eb9df813a7
Jagdeep Singh Kalsi <jdecode@gmail.com>
2025-12-21 20:18:19 +0530

    feat: Integrate Laravel Telescope, update Composer dependencies, refine GitHub workflows, and add a base application layout.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class) && app()->environment('local') ? App\Providers\TelescopeServiceProvider::class : null,
 ];
